5d54a6fb2200fee3efb1419c5f0b055044207121
Jack Shelley <email": null>
2016-12-08 10:53:15 -0800

    Added ability for user to own games

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,6 @@
 class Game < ActiveRecord::Base
+  has_and_belongs_to_many :users
+
   has_many :comments, as: :commentable
   has_many :votes, as: :votable
 end
---
+++ db/migrate/20161208181651_create_games_users.rb
@@ -0,0 +1,5 @@
+class CreateGamesUsers < ActiveRecord::Migration
+  def change
+    create_join_table :games, :users
+  end
+end
