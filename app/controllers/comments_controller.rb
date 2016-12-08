9de656a6c11c4db2b8a2b160e4895289194a38b0
Kelsey Edelstein <email": null>
2016-12-08 15:18:47 -0800

    Implement comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,16 @@
 class CommentsController < ApplicationController
 
   def new
+    @game = Game.find(params[:game_id])
     @comment = Comment.new
   end
 
   def create
-    @game = Game.find(params[:id])
-    @user = User.find(session[:user_id])
+    @game = Game.find(params[:game_id])
+    @user = User.find(session["warden.user.user.key"][0][0])
     @comment = @user.comments.new(comment_params, commentable_id: @game.id, author_id: @user.id, commentable_type: "Game")
     if @comment.save
-      redirect_to game_comments_path(@comment)
+      redirect_to game_comments_path(@game)
     else
       render 'new'
     end
