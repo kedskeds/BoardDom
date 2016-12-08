b028826cba42137228e0b60ca3cf899a43ccd453
Cory Mathis <email": null>
2016-12-07 19:00:26 -0800

    Devise implemented

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # Gives devise a default mailer url
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
