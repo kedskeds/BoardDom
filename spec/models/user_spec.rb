b1c40ee67ea3ed4638b59d437b6b52612e3c3329
Cory Mathis <email": null>
2016-12-08 14:03:17 -0800

    Add tests for user model.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "user is valid" do
+    it "has a username" do
+      user = User.create(username: "Fred", email: "fred@gmail.com", password: "password", password_confirmation: "password" )
+      expect(user.username).to eq("Fred")
+    end
+
+    it "has an email" do
+      user = User.create(username: "Fred", email: "fred@gmail.com", password: "password", password_confirmation: "password" )
+      expect(user.email).to eq("fred@gmail.com")
+    end
+
+    it "has a valid password" do
+      user = User.create(username: "Fred", email: "fred@gmail.com", password: "password", password_confirmation: "password" )
+      expect(user.valid_password?("password")).to eq(true)
+    end
+
+    it "is saved in the database" do
+      user = User.create(username: "Fred", email: "fred@gmail.com", password: "password", password_confirmation: "password" )
+      last_user = User.last
+      expect(last_user).to eq(user)
+    end
+  end
+
+  context "user is invalid" do
+
+    it "is not saved in the data base" do
+      user = User.create(username: "", email: "fred@gmail.com", password: "password", password_confirmation: "paswsord" )
+      last_user = User.last
+      expect(last_user).not_to eq(user)
+    end
+  end
 end
