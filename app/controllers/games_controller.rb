d82516e62b21c0ca7906e3f8f4c0df6dad29e865
Cory Mathis <email": null>
2016-12-09 12:03:28 -0800

    changed controller stuff

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,7 +2,8 @@ class GamesController < ApplicationController
   def index
     @games_alphabetically = Game.order(:title)
     (@owned_games = current_user.games.all) if user_signed_in?
-    @games_by_rating = Game.all.sort{|game1, game2| game1.votes.where(up: 1).count <=> game2.votes.where(up: 1).count}
+
+    @games_by_rating = Game.all.sort_by {|game| -game.votes.where(up: true).count }
     @scifi_games = Game.where(genre: 'Sci-Fi')
     @strategy = Game.where(category: 'Table Top Strategy')
   end
