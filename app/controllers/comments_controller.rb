85de7fb908059546ace0c5e0bd30c9a67a6e0522
Kelsey Edelstein <email": null>
2016-12-08 16:11:02 -0800

    Fix bug in comment form

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,9 +8,10 @@ class CommentsController < ApplicationController
   def create
     @game = Game.find(params[:game_id])
     @user = User.find(session["warden.user.user.key"][0][0])
-    @comment = @user.comments.new(comment_params, commentable_id: @game.id, author_id: @user.id, commentable_type: "Game")
+
+    @comment = Comment.new(comment_params.merge(commentable_id: @game.id, author_id: @user.id, commentable_type: "Game"))
     if @comment.save
-      redirect_to game_comments_path(@game)
+      redirect_to @game
     else
       render 'new'
     end
